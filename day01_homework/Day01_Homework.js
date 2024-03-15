2c00c9ac5c3bdc76f65ff8d572f1ae623e2938b0
GuramiPapunashvili <157598807+GuramiPapunashvili@users.noreply.github.com>
2024-03-16 02:18:10 +0400

    Add files via upload

---
+++ day01_homework/Day01_Homework.js
@@ -0,0 +1,27 @@
+let row = 4;
+let col = 5;
+
+const getTable = (row, col) => {
+    let table = []
+    for (let i = 0; i < row; i++) {
+    
+        
+    for (let x = 0; x < col; x++) {
+    //.....
+    }
+
+        
+    return table;
+
+}
+}
+
+console.log(getTable(row, col));
+
+// Output:
+// [
+//  [ 1, 8, 9,  16, 17 ],
+//  [ 2, 7, 10, 15, 18 ],
+//  [ 3, 6, 11, 14, 19 ],
+//  [ 4, 5, 12, 13, 20 ]
+// ]
